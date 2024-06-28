000bdca166ef8821d5f92dd061926af9475d1593
Mariana Avila <marianaavilaa64@gmail.com>
2024-06-28 11:59:18 -0400

    database and models

---
+++ server/models/state.js
@@ -0,0 +1,33 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class State extends Model {
+    static associate(models) {
+      // Define associations here
+      this.hasMany(models.City,{foreignKey: 'stateId'})
+    }
+  }
+
+  State.init({
+    id:{
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+     type:  DataTypes.STRING,
+    },
+    abbreviation:{
+     type: DataTypes.STRING
+    }
+   
+  }, {
+    sequelize,
+    tableName:'states',
+    modelName: 'State',
+  });
+
+  return State;
+};
