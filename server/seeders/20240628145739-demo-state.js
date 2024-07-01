e11d4762cf13adac964111a575030728271b7ee8
Mariana Avila <marianaavilaa64@gmail.com>
2024-07-01 13:31:28 -0400

    data added

--- server/seeders/20240628145739-demo-state.js
+++ server/seeders/20240628145739-demo-state.js
@@ -1,13 +1,22 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('states', [
-      {
-        id: 1,
-        name: 'New York',
-        abbreviation: 'NY',
-        
-      },
-    ]);
+  up: async (queryInterface, Sequelize) => {
+    const states = [
+      { id: 1, name: 'New York', abbreviation: 'NY' },
+      { id: 2, name: 'Washington', abbreviation: 'W' },
+      { id: 3, name: 'Nebraska', abbreviation: 'N' },
+    ];
+
+    for (const state of states) {
+      const existingState = await queryInterface.rawSelect('states', {
+        where: {
+          id: state.id,
+        },
+      }, ['id']);
+
+      if (!existingState) {
+        await queryInterface.bulkInsert('states', [state], {});
+      }
+    }
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('states', null, {});
