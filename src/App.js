5376dc38dd0d0c4dabc7c6e555bbeecebe97a257
Mariana Avila <marianaavilaa64@gmail.com>
2024-07-01 14:58:58 -0400

    testing frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, {useEffect, useState} from 'react';
 import Home from "./pages/Home";
 import SearchResult from "./pages/SearchResult";
 import Search from "./pages/Search";
 
 export default function App() {
+  const [backendData, setBackendData]= useState([{}])
+  useEffect(() => {
+
+  fetch("/api").then(
+    response => response.json()
+  ).then(
+    data => {
+      setBackendData(data)
+    }
+  )
+} ,[])
   return (
     <BrowserRouter>
       <Routes>
