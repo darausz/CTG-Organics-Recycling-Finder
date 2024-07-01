36170099cd7cfbe92c6f65d3b8a428d899a70f13
Mariana Avila <marianaavilaa64@gmail.com>
2024-07-01 17:07:13 -0400

    testing backend+ frontend

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express= require('express')
-const app= express()
+const app= express();
+app.use(express.json());
 
 app.get("/api",(req,res)=>{
     res.json({"users": ["userOne","userTwo"]})
--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Home from "./pages/Home";
 import SearchResult from "./pages/SearchResult";
 import Search from "./pages/Search";
 
 export default function App() {
-  const [backendData, setBackendData]= useState([{}])
-  useEffect(() => {
+  const [backendData, setBackendData] = useState([{}]);
 
-  fetch("/api").then(
-    response => response.json()
-  ).then(
-    data => {
-      setBackendData(data)
-    }
-  )
-} ,[])
+  useEffect(() => {
+    fetch("/api")
+      .then(response => response.json())
+      .then(data => {
+        setBackendData(data);
+      });
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route index element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/search/result" element={<SearchResult />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <div>
+        {backendData.users && backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))}
+      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/search/result" element={<SearchResult />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
-
