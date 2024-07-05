84b8ba36de3d4ca8af1c47d8f4d5f53898822744
Mariana Avila <marianaavilaa64@gmail.com>
2024-07-05 09:38:10 -0400

    added routes

--- server/controllers/stateController.js
+++ server/controllers/stateController.js
@@ -39,4 +39,36 @@ exports.createState= async(req,res)=>{
         return res.json(err)
 
     }
-};
+}
+//update state
+exports.updateState= async(req,res) =>{
+    let stateId= req.params.id
+    try{
+        let state=await State.findOne({
+            where:{ 
+                id: stateId
+            }
+        })
+        if(!state){
+            return res.status(404).json({"Message":"Unable to find state with that ID"})
+        }
+        //list of attributes that should not be modified
+        const excludeAttributes=['id']
+        const filteredAttributes= Object.keys(req.body).reduce((acc,key)=>{
+            if (!excludeAttributes.includes(key)){
+                acc[key]= req.body[key];
+            }
+            return acc;
+        }, {});
+        //if there are no valid attributes to update then throw error 
+        if(Object.keys(filteredAttributes).length===0){
+            return res.json({"Message": "Unable to update any attributes"})
+        }
+
+        state=await state.update(filteredAttributes)
+        return res.status(200).json()
+    }catch(err){
+        console.log(err)
+        return res.status(400).json(err)
+    }
+}
