57384b2020709fd9a09de739dcedfc762e0dd619
marianaavilaa64@gmail.com <marianaavilaa64@gmail.com>
2024-07-23 16:18:00 -0400

    Included LocalStorage. Map Not working correctly

--- src/components/smartBinsProvider.js
+++ src/components/smartBinsProvider.js
@@ -1,10 +1,22 @@
-import React, { useState, useContext, createContext } from "react";
+import React, { useState, useContext, createContext,useEffect } from "react";
 
 const SmartBinContext= createContext();
 
 export function SmartBinsProvider({children}){
     const [smartBins, setSmartBins] = useState([]);
     const [singleSmartBins, setSingleSmartBins]= useState({});
+    useEffect(() => {
+        const savedSmartBins = localStorage.getItem('smartBins');
+        const savedSingleSmartBins = localStorage.getItem('singleSmartBins');
+    
+        if (savedSmartBins) setSmartBins(JSON.parse(savedSmartBins));
+        if (savedSingleSmartBins) setSingleSmartBins(JSON.parse(savedSingleSmartBins));
+      }, []);
+    
+      useEffect(() => {
+        localStorage.setItem('smartBins', JSON.stringify(smartBins));
+        localStorage.setItem('singleSmartBins', JSON.stringify(singleSmartBins));
+      }, [smartBins, singleSmartBins]);
 
     return(
         <SmartBinContext.Provider value ={{smartBins,setSmartBins,singleSmartBins,setSingleSmartBins}}>
