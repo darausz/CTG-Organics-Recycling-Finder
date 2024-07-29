bfaac8085d60fd7e41ee1fec9c4c2f8964cbcfa2
marianaavilaa64@gmail.com <marianaavilaa64@gmail.com>
2024-07-29 10:49:49 -0400

    commented LS out

--- src/components/smartBinsProvider.js
+++ src/components/smartBinsProvider.js
@@ -3,30 +3,19 @@ import React, { useState, useContext, createContext,useEffect } from "react";
 const SmartBinContext= createContext();
 
 export function SmartBinsProvider({children}){
-  const storedSmartBins=localStorage.getItem('smartBins');
+  /* const storedSmartBins=localStorage.getItem('smartBins');
   let parsedSmartBins;
   if(storedSmartBins){
     parsedSmartBins= JSON.parse(storedSmartBins);
   }else{
     parsedSmartBins= [];
-  }
-    const [smartBins, setSmartBins] = useState(parsedSmartBins);
+  } */
+
+    const [smartBins, setSmartBins] = useState([]);
     const [singleSmartBins, setSingleSmartBins]= useState({});
-    useEffect(()=>{
+    /* useEffect(()=>{
       localStorage.setItem('smartBins',JSON.stringify(smartBins))
-    },[smartBins])
-   /*  useEffect(() => {
-        const savedSmartBins = localStorage.getItem('smartBins');
-        const savedSingleSmartBins = localStorage.getItem('singleSmartBins');
-    
-        if (savedSmartBins) setSmartBins(JSON.parse(savedSmartBins));
-        if (savedSingleSmartBins) setSingleSmartBins(JSON.parse(savedSingleSmartBins));
-      }, []);
-    
-      useEffect(() => {
-        localStorage.setItem('smartBins', JSON.stringify(smartBins));
-        localStorage.setItem('singleSmartBins', JSON.stringify(singleSmartBins));
-      }, [smartBins, singleSmartBins]); */
+    },[smartBins]) */
 
     return(
         <SmartBinContext.Provider value ={{smartBins,setSmartBins,singleSmartBins,setSingleSmartBins}}>
